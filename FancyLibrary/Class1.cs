076631d1864b4c563ce76fb445a4cb9ccfc7e456
ahorinka <ahorinka@infinitrac.com>
2020-02-05 15:55:52 +0100

    init

---
+++ FancyLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FancyLibrary
+{
+    public class FancyService
+    {
+        public void FirstFancyMessage()
+        {
+            Console.WriteLine("Initial fancy message is being sent from submodule..\n");
+        }
+    }
+}
